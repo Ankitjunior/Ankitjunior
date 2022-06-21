3d4c79c3281f71df4f1c0fd7fad650062e6562b7
Ankitjunior <107884313+Ankitjunior@users.noreply.github.com>
2022-06-21 07:01:54 +0530

    Question 1.c

---
+++ Question 1.c
@@ -0,0 +1,11 @@
+/*c program to sum of two numbers*/
+#include<stdio.h>
+int main
+{
+int x y,z;
+printf("enter two number\n");
+scanf("%d%d"'&x,&y);
+z=x+y;
+printf("sum of %d and %d is %d",x,y,z);
+ getch();
+ }
