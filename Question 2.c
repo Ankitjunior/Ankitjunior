abd83388eca293dd2662e3c898de10c368ed236a
Ankitjunior <107884313+Ankitjunior@users.noreply.github.com>
2022-06-21 07:23:42 +0530

    Create Question 2.c

---
+++ Question 2.c
@@ -0,0 +1,19 @@
+/* c program to calculate  area of circle*/
+#include<tdio.h>
+int main()
+  {
+  int r;
+  float a;
+  printf("enter the radius of circlein cm");
+scanf("%d",&r);
+  a=3.14*r*r;
+  printf("area of circl  of radius %d will be %f cm*cm",r,a);
+  getch();
+  }
+
+
+    
+
+
+  
+  
