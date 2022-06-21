bcc93ea6fa8f52aa1410b485a84de38a28ed2db6
Ankitjunior <107884313+Ankitjunior@users.noreply.github.com>
2022-06-21 08:07:19 +0530

    Create Question 3.c

---
+++ Question 3.c
@@ -0,0 +1,14 @@
+/* c program to calculate volume of a cuboid*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+  {
+  int l,b,h,v;
+  printf("enter the length,breadth and height of cuboid in centimetr");
+
+
+scanf("%d%d%d",&a,&b,&c);
+  v=l*b*h;
+  printf("volume of cubio will be %d",v);
+  getch();
+  
